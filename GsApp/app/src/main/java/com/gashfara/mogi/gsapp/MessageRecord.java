d47d4cfeb341a417ee60e941f8469d5ab08ad2b1
mogiken <mogi@hugg.co.jp>
2015-08-04 16:10:52 +0900

    good完成

--- GsApp/app/src/main/java/com/gashfara/mogi/gsapp/MessageRecord.java
+++ GsApp/app/src/main/java/com/gashfara/mogi/gsapp/MessageRecord.java
@@ -2,22 +2,26 @@
 package com.gashfara.mogi.gsapp;
 
 //ListView2ではデータクラスにidを追加してください。
+//GoodではデータクラスにgoodCountを追加してください。。
 public class MessageRecord {
     //保存するデータ全てを変数で定義します。
     private String imageUrl;
     private String comment;
     private String id;
+    private int goodCount;
 
     //データを１つ作成する関数です。項目が増えたら増やしましょう。
-    public MessageRecord(String id,String imageUrl, String comment) {
+    public MessageRecord(String id,String imageUrl, String comment,int goodCount) {
         this.imageUrl = imageUrl;
         this.comment = comment;
         this.id = id;
+        this.goodCount = goodCount;
     }
     //それぞれの項目を返す関数です。項目が増えたら増やしましょう。
     public String getComment() {return comment;}
     public String getImageUrl() {return imageUrl;}
-    public String getId() {
-        return id;
-    }
+    public String getId() {return id;}
+    public int getGoodCount() {return goodCount;}
+    //セットする関数.項目が増えたら追加しましょう
+    public void setGoodCount(int goodCount){this.goodCount = goodCount;}
 }
--- GsApp/app/src/main/java/com/gashfara/mogi/gsapp/MessageRecordsAdapter.java
+++ GsApp/app/src/main/java/com/gashfara/mogi/gsapp/MessageRecordsAdapter.java
@@ -5,17 +5,21 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.text.Spannable;
-import android.text.method.LinkMovementMethod;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.NetworkImageView;
+import com.kii.cloud.storage.KiiObject;
+import com.kii.cloud.storage.callback.KiiObjectCallBack;
 
 import java.util.List;
 
@@ -31,8 +35,9 @@ public class MessageRecordsAdapter extends ArrayAdapter<MessageRecord> {
         mImageLoader = new ImageLoader(VolleyApplication.getInstance().getRequestQueue(), new BitmapLruCache());
     }
     //表示するViewを返します。これがListVewの１つのセルとして表示されます。表示されるたびに実行されます。
+    //Goodで修正。int positionをfinal int positionにしてください。コールバック関数でpostionの値がほしいので。
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, ViewGroup parent) {
         //convertViewをチェックし、Viewがないときは新しくViewを作成します。convertViewがセットされている時は未使用なのでそのまま再利用します。メモリーに優しい。
         if(convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.message_item, parent, false);
@@ -90,6 +95,55 @@ public class MessageRecordsAdapter extends ArrayAdapter<MessageRecord> {
         imageView.setImageUrl(imageRecord.getImageUrl(), mImageLoader);
         //Viewに文字をセットします。
         textView.setText(imageRecord.getComment());
+
+        //Goodで追加ここから　
+        //いいねボタンを得る
+        Button buttonView = (Button) convertView.findViewById(R.id.button1);
+        //ボタンの文字にいいねの数を追加します。
+        buttonView.setText(getContext().getString(R.string.good)+":"+imageRecord.getGoodCount());
+
+        //ボタンを押した時のクリックイベントを定義
+        buttonView.setOnClickListener(new OnClickListener() {
+            //クリックした時
+            @Override
+            public void onClick(View view) {
+                //いいねボタンを得る
+                Button buttonView = (Button) view;
+                ////タグからどの位置のボタンかを得る
+                //int position = (Integer)buttonView.getTag();
+                //MessageRecordsAdapterの位置からMessageRecordのデータを得る
+                MessageRecord messageRecord =  getItem(position);
+                //messagesのバケット名と_idの値からKiiObjectのuri(データの場所)を得る。参考：http://documentation.kii.com/ja/starts/cloudsdk/cloudoverview/idanduri/
+                Uri objUri = Uri.parse("kiicloud://buckets/" + "messages" + "/objects/" + messageRecord.getId());
+                //uriから空のデータを作成
+                KiiObject object = KiiObject.createByUri(objUri);
+                //いいねを＋１する。
+                object.set("goodCount", messageRecord.getGoodCount()+ 1);
+                //既存の他のデータ(_id,comment,imageUrlなど)はそのままに、goodCountだけが更新される。参考：http://documentation.kii.com/ja/guides/android/managing-data/object-storages/updating/#full_update
+                object.save(new KiiObjectCallBack() {
+                    //KiiCloudの更新が完了した時
+                    @Override
+                    public void onSaveCompleted(int token, KiiObject object, Exception exception) {
+                        if (exception != null) {
+                            //エラーの時
+                            return;
+                        }
+                        //MessageRecordsAdapterの位置からMessageRecordのデータを得る
+                        MessageRecord messageRecord =  getItem(position);
+                        //messageRecordのいいねの数を+1する。これでKiiCloudの値とListViewのデータが一致する。
+                        messageRecord.setGoodCount(messageRecord.getGoodCount()+1);
+                        //データの変更を通知します。
+                        notifyDataSetChanged();
+                        //トーストを表示.Activityのコンテキストが必要なのでgetContext()してる。
+                        Toast.makeText(getContext(), getContext().getString(R.string.good_done), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+            }
+        });
+        //Goodで追加ここまで
+
+
         //1つのセルのViewを返します。
         return convertView;
     }
@@ -104,4 +158,5 @@ public class MessageRecordsAdapter extends ArrayAdapter<MessageRecord> {
         //データの変更を通知します。
         notifyDataSetChanged();
     }
+
 }
