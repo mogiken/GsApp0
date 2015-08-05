6ab150b565eb240d5375860c3b7b1db250a7cd05
mogiken <mogi@hugg.co.jp>
2015-08-05 12:53:43 +0900

    とりあえず

--- GsApp/app/src/main/java/com/gashfara/mogi/gsapp/VolleyApplication.java
+++ GsApp/app/src/main/java/com/gashfara/mogi/gsapp/VolleyApplication.java
@@ -8,6 +8,7 @@ import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
 import com.google.android.gms.analytics.GoogleAnalytics;
 import com.google.android.gms.analytics.Tracker;
+import com.kii.cloud.analytics.KiiAnalytics;
 import com.kii.cloud.storage.Kii;
 
 import java.util.HashMap;
@@ -61,6 +62,8 @@ public class VolleyApplication extends Application {
         //KiiCloudの初期化。Applicationクラスで実行してください。キーは自分の値にかえる。
         Kii.initialize(getApplicationContext(), "80dd6a22", "f74778c9d5280992913b93ed39ec0f5a", Kii.Site.JP);
         //Userで追加ここまで
+        KiiAnalytics.initialize(this, "80dd6a22", "f74778c9d5280992913b93ed39ec0f5a", KiiAnalytics.Site.JP);
+
     }
     //インスタンスを返す関数（メソッドです）。クラスの中にある変数はこのように関数を通じて返すようにするのが一般的です。
     //synchronizedは同時に動作すると不具合が起きるときに宣言します。Volleyの仕様です。
